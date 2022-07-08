ada27b0b70810003e88f993cdde69bf20a1fd6f6
colehammerle <synicvl@gmail.com>
2022-07-07 23:39:38 -0400

    commit tree 6de7faaf38f8a1eea18b5cca9e9d56d3c4794f42

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(`HELLO`)
+}
+function logWhisper(string) {
+  console.log (`hello`)
+}
+function sayHiToHeadphonedRoommate(string) {
+let lower = "hello"
+let upper = "HELLO"
+let dinner = "Let's have dinner together!"
+let deaf = "I can't hear you!"
+let YES = "YES INDEED!"
+let love = "I would love to!"
+console.log (string)(lower, upper, dinner)
+return (string)(deaf, YES, love)
+}
